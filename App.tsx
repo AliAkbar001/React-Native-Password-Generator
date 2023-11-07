f257779d38274bec65de1f6a065baf7b458967d5
DESKTOP-5T0E1CO\AliAkbar <786aliakbar.aa@gmail.com>
2023-11-08 02:55:35 +0500

    Create Logic

--- App.tsx
+++ App.tsx
@@ -1,7 +1,52 @@
 import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function App() {
+  const [password, setPassword] = useState('')
+  const [isPasswordGenerated, setIsPasswordGenerated] = useState(false)
+  const [lowerCase, setLowerCase] = useState(false)
+  const [upperCase, setUpperCase] = useState(false)
+  const [numbers, setNumbers] = useState(false)
+  const [symbols, setSymbols] = useState(false)
+
+  const generatePassword = (passwordLength:number) => {
+    let characters = ''
+    const upperCaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    const lowerCaseChars = "abcdefghijklmnopqrstuvwxyz"
+    const numbersChars = "0123456789"
+    const specialChars = "!@#$%^&*()_+[]\';/.,{}|:?> <`~"
+
+    if(upperCase){
+      characters += upperCaseChars
+    }
+    if(lowerCase){
+      characters += lowerCaseChars
+    }
+    if(numbers){
+      characters += numbersChars
+    }
+    if(symbols){
+      characters += specialChars
+    }
+
+    let result = ''
+    for (let i = 0; i < passwordLength; i++) {
+      const characterIndex = Math.round(Math.random() * characters.length)
+      result += characters.charAt(characterIndex)
+    }
+
+    return result;
+  }
+
+  const resetStates = () =>{
+    setPassword('')
+    setIsPasswordGenerated(false)
+    setLowerCase(false)
+    setNumbers(false)
+    setUpperCase(false)
+    setSymbols(false)
+  }
+  
   return (
     <View>
       <Text>App</Text>
